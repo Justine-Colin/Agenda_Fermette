47ceb2e4b6935d6f572605de3cffd24607903867
Justine-Colin <43772781+Justine-Colin@users.noreply.github.com>
2018-11-18 20:38:51 +0100

    Init commit

--- Agenda_Fermette/MainWindow.xaml.cs
+++ Agenda_Fermette/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace Agenda_Fermette
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Il faut s'arranger pour gérer la taille des éléments en relatif
+
         public MainWindow()
-        {
+        {            
             InitializeComponent();
+            AfficherJourBouton();
+        }
+
+        public void AfficherJourBouton()
+        //Change le nom des jours sur les boutons selon le jour d'ajd
+        {
+            DateTime Ajd = new DateTime();
+            //Récupère la date du jour
+            Ajd = DateTime.Now;
+            //Notifie le programme que la langue à utiliser est le français
+            var Langue = new System.Globalization.CultureInfo("fr-FR");
+            //Boucle pour la semaine
+            for(int i = 0; i < 7; i++)
+            {
+                /*Va rechercher le jour d'aujourd'hui en anglais avec DayOfWeek
+                 * Puis ajoute i pour arriver au jour souhaité
+                 * puis traduit en français 
+                 */
+                var Jour = Langue.DateTimeFormat.GetDayName(Ajd.DayOfWeek + i);
+                //Ajoute les jours pour changer la date
+                DateTime Date = Ajd.AddDays(i);
+                //Remplace la première lettre par une majuscule et ajoute la date derrière /!\ MM donne le mois mm donne les minutes
+                Jour = char.ToUpper(Jour[0]) + Jour.Substring(1) + " " + Date.ToString("dd/MM");
+                //Prépare le string de la marge
+                //Crée le bouton correspondant
+                Button Bouton = new Button
+                {
+                    Name = "Jour_" + i,                    
+                    //Le place sur la page
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Width = 274,
+                    Height = 50,
+                    Visibility = Visibility.Visible
+                };
+                //Remplit avec le jour qu'il faut
+                Bouton.Content = Jour;
+                Semaine.Children.Add(Bouton);
+            } 
         }
     }
 }
